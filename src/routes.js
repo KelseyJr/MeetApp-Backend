fd70dd3f4c49317cf272f524bffc3b81a3b8d95f
Kelsey Junior <kelseytfjunior@gmail.com>
2019-07-04 15:16:46 -0300

    Organizer controller

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import MeetUpController from './app/controllers/MeetUpController';
 import SubscriptionController from './app/controllers/SubscriptionController';
+import OrganizingController from './app/controllers/OrganizingController';
 
 import authMiddleware from './app/middlewares/auth';
 import multerConfig from './config/multer';
@@ -27,5 +28,7 @@ routes.put('/meetup/:id', MeetUpController.update);
 routes.delete('/meetup/:id', MeetUpController.delete);
 
 routes.post('/subscription/:id', SubscriptionController.store);
+
+routes.get('/organizing', OrganizingController.index);
 
 export default routes;
